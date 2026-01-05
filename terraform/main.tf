c8f2e46df0e0284002ae587b7e70827f32089d75
devangnpm <d3vang2002@gmail.com>
2026-01-06 01:28:41 +0530

    key pair generation and attach to ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,36 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# 1. Generate the RSA Private Key
+resource "tls_private_key" "main_key" {
+algorithm = "RSA"
+rsa_bits  = 4096
+}
+
+# 2. Register the Public Key with AWS
+resource "aws_key_pair" "deployer" {
+key_name   = "my-terraform-key"
+public_key = tls_private_key.main_key.public_key_openssh
+}
+
+# 3. Save the Private Key to your local machine
+resource "local_file" "ssh_key" {
+filename        = "${pathexpand("~/.ssh/aws-key.pem")}"
+content         = tls_private_key.main_key.private_key_pem
+file_permission = "0400" # Important: Sets read-only permission for security
+}
+
 
 // new aws instance
 resource "aws_instance" "bot_instance" {
   ami           = "ami-02b8269d5e85954ef" // amazon ami image id (ubuntu 64 bit architecture)
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   # user_data     = file("../scripts/startup.sh")
-  user_data = templatefile("${path.module}/scripts/setup_runner.sh", {
+  user_data = templatefile("${path.module}/scripts/startup.sh", {
     runner_token = var.gh_runner_token
   })
+  key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.bot_firewall.id]
 
   // dashboard instance name
   tags = {
@@ -18,11 +39,16 @@ resource "aws_instance" "bot_instance" {
   }
 }
 
+// use default vpc alread created
+data "aws_vpc" "default" {
+  default = true
+}
+
 // defining our security group here for allowed traffic on our server defined ports
 resource "aws_security_group" "bot_firewall" {
   name        = "bot-firewall"
   description = "Allow SSH and Web traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = data.aws_vpc.default.id
 }
 
 // open ssh port
@@ -30,7 +56,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.bot_firewall.id
 
   # ssh allowed only from my ip
-  cidr_ipv4   = "${var.my_ip}/32"
+  cidr_ipv4   = "0.0.0.0/0"
   from_port   = 22
   ip_protocol = "tcp"
   to_port     = 22
@@ -45,11 +71,9 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   to_port           = 80
 }
 
-# outbound Rule allow limited https traffic
-resource "aws_vpc_security_group_egress_rule" "allow_https_outbound" {
+# outbound Rule allow all outbound traffic
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
   security_group_id = aws_security_group.bot_firewall.id
-  cidr_ipv4         = "0.0.0.0/0" // to any ip address
-  ip_protocol       = "tcp"       // only https allowed
-  from_port         = 443
-  to_port           = 443
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
--- terraform/scripts/startup.sh
+++ terraform/scripts/startup.sh
@@ -1,45 +1,56 @@
 #!/usr/bin/env bash
 
+
+
 # ec2 startup script to setup every thing
+
+set -euxo pipefail
+
+echo "EC2 startup script started"
 
 # update system packages
 echo "startup script started!"
-sudo apt-get update -y
-sudo apt-get upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
 echo "packages update complete"
 
 # install tools required
 # Add Docker's official GPG key:
-sudo apt update
-sudo apt install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+apt update
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
-Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Suites: $(. /etc/os-release && echo "$${UBUNTU_CODENAME:-$${VERSION_CODENAME}}")
 Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-sudo apt update
+apt update
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# start docker and enable it on boot
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo systemctl status docker
+# start docker and enable it on every boot
+systemctl start docker
+systemctl enable docker
+systemctl status docker
+
+# adding ubuntu user to docker group to run without sudo in ssh
+usermod -aG docker ubuntu
 
 # Create a folder
-$ mkdir actions-runner && cd actions-runner# Download the latest runner package
-$ curl -o actions-runner-linux-x64-2.330.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.330.0/actions-runner-linux-x64-2.330.0.tar.gz# Optional: Validate the hash
-$ echo "af5c33fa94f3cc33b8e97937939136a6b04197e6dadfcfb3b6e33ae1bf41e79a  actions-runner-linux-x64-2.330.0.tar.gz" | shasum -a 256 -c# Extract the installer
-$ tar xzf ./actions-runner-linux-x64-2.330.0.tar.gz
+mkdir actions-runner && cd actions-runner# Download the latest runner package
+curl -o actions-runner-linux-x64-2.330.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.330.0/actions-runner-linux-x64-2.330.0.tar.gz# Optional: Validate the hash
+echo "af5c33fa94f3cc33b8e97937939136a6b04197e6dadfcfb3b6e33ae1bf41e79a  actions-runner-linux-x64-2.330.0.tar.gz" | shasum -a 256 -c# Extract the installer
+tar xzf ./actions-runner-linux-x64-2.330.0.tar.gz
 
 # Create the runner and start the configuration experience
-$ ./config.sh --url https://github.com/devangy/market --token ${runner_token} # Last step, run it!
-$ ./run.sh
+./config.sh --url https://github.com/devangy/market --token ${runner_token} # Last step, run it!
+./run.sh
+
+echo "EC2 startup script completed"
